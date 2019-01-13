e9abca2c2a6a14d3d748aec9da305dcb0c19fd00
liuchang <liu930505@qq.com>
2019-01-13 22:50:41 +0800

    halo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,33 @@ import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
 import Header from './Header'
 import Content from './Content'
-import './index.css'
+
+const createStore = (reducer) => {
+  let state = null
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+  dispatch({})
+  return {getState, dispatch, subscribe}
+}
+
+const themeReducer = (state, action) => {
+  if(!state) return {
+    themeColor: 'red'
+  }
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, themeColor: action.themeColor }
+    default:
+      return state
+  }
+}
+
+const store = createStore(themeReducer)
 
 class Index extends Component {
   render () {
@@ -16,7 +42,4 @@ class Index extends Component {
   }
 }
 
-ReactDOM.render(
-  <Index />,
-  document.getElementById('root')
-)
+ReactDOM.render(<Index />, document.getElementById('root'))
